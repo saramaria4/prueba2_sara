c26c5b42397171ee0864a294128261acef457055
sara <sara@nanomia.cl>
2016-11-09 16:41:49 -0300

    scopes agregados

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
 	has_many :review
+	scope :ranking, -> { where("ranking < ?", 3) }
+	scope :review, -> { where(review: false) }
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
-  has_many :movie
   validates_presence_of :description
                     :length => { :maximun => 255 }
 end
