72158a38516662f8ffb4c074326015dec0e03cce
sara <sara@nanomia.cl>
2016-11-09 16:25:52 -0300

    Diagrama rails roady y relaciones check

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
+  has_many :movie
   validates_presence_of :description
                     :length => { :maximun => 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :review
 	validates_presence_of :name
 	validates_presence_of :email
 	validates :email, uniqueness: true
