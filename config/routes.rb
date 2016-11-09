cee156ea3dfede0c5fe44a9ccf49c53a4b0085f9
sara <sara@nanomia.cl>
2016-11-09 17:43:40 -0300

    gemas heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  
+  get  'user/home'
+  get  'movie/home'
+  get  'review/home' 
+
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
